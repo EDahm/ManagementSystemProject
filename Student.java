2f067a2941009bf9034044ea5cbe582106396553
EDahm <74915490+EDahm@users.noreply.github.com>
2020-12-23 12:11:37 +0900

    1223

---
+++ Student.java
@@ -0,0 +1,33 @@
+package Project;
+
+public class Student {
+
+	String name;
+	String gender;
+	String birth;
+	String phnum;
+	String type;
+	int lecture;
+	int score;
+
+	
+	public Student(String name, String gender, String birth, String phnum, String type, int lecture, int score) {
+		this.name = name;
+		this.gender = gender;
+		this.birth = birth;
+		this.phnum = phnum;
+		this.type = type;
+		this.lecture = lecture;
+		this.score = score;
+	}
+
+	
+	
+	
+
+	
+	
+	
+
+	
+}
